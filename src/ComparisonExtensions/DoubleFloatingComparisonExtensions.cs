dc04debf1f37c1ec59b22750e3ff27e54b6c46c8
Zach Langner <zlangner@gmail.com>
2023-07-23 12:43:17 -0500

    refactor: split extension class into 2 files

--- src/ComparisonExtensions/DoubleFloatingComparisonExtensions.cs
+++ src/ComparisonExtensions/DoubleFloatingComparisonExtensions.cs
@@ -2,93 +2,93 @@
 
 namespace ComparisonExtensions
 {
-    public static class FloatFloatingComparisonExtensions
+    public static class DoubleFloatingComparisonExtensions
     {
-        private const float FloatPrecision = (float) 0.0000001;
-                
+        private const double DoublePrecision = 0.000000000001;
+
         /// <summary>
         /// Checks for equality of two numbers with a certain precision value.
         /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         // ReSharper disable once InconsistentNaming
-        public static bool EqualsTo(this float left, float right)
+        public static bool EqualsTo(this double left, double right)
         {
-            if (float.IsPositiveInfinity(left) && float.IsPositiveInfinity(right))
+            if (double.IsPositiveInfinity(left) && double.IsPositiveInfinity(right))
             {
                 return true;
             }
 
-            if (float.IsNegativeInfinity(left) && float.IsNegativeInfinity(right))
+            if (double.IsNegativeInfinity(left) && double.IsNegativeInfinity(right))
             {
                 return true;
             }
 
-            return left - right is < FloatPrecision and > -FloatPrecision;
+            return left - right is < DoublePrecision and > -DoublePrecision;
         }
 
         /// <summary>
         /// Checks if the number is Zero with a certain precision value.
         /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool IsZero(this float value)
+        public static bool IsZero(this double value)
         {
-            return value is < FloatPrecision and > -FloatPrecision;
+            return value is < DoublePrecision and > -DoublePrecision;
         }
 
         /// <summary>
         /// Checks if the number is greater than the second number with a certain precision value.
         /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool IsGreaterThan(this float left, float right)
+        public static bool IsGreaterThan(this double left, double right)
         {
-            return left - right > FloatPrecision;
+            return left - right > DoublePrecision;
         }
 
         /// <summary>
         /// Checks if the number is greater than or equals to the second number with a certain precision value.
         /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool IsGreaterThanOrEquals(this float left, float right)
+        public static bool IsGreaterThanOrEquals(this double left, double right)
         {
-            if (float.IsPositiveInfinity(left) && float.IsPositiveInfinity(right))
+            if (double.IsPositiveInfinity(left) && double.IsPositiveInfinity(right))
             {
                 return true;
             }
 
-            if (float.IsNegativeInfinity(left) && float.IsNegativeInfinity(right))
+            if (double.IsNegativeInfinity(left) && double.IsNegativeInfinity(right))
             {
                 return true;
             }
 
-            return left - right > -FloatPrecision;
+            return left - right > -DoublePrecision;
         }
 
         /// <summary>
         /// Checks if the number is less than the second number with a certain precision value.
         /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool IsLessThan(this float left, float right)
+        public static bool IsLessThan(this double left, double right)
         {
-            return right - left > FloatPrecision;
+            return right - left > DoublePrecision;
         }
 
         /// <summary>
         /// Checks if the number is less than or equals to the second number with a certain precision value.
         /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool IsLessThanOrEquals(this float left, float right)
+        public static bool IsLessThanOrEquals(this double left, double right)
         {
-            if (float.IsPositiveInfinity(left) && float.IsPositiveInfinity(right))
+            if (double.IsPositiveInfinity(left) && double.IsPositiveInfinity(right))
             {
                 return true;
             }
 
-            if (float.IsNegativeInfinity(left) && float.IsNegativeInfinity(right))
+            if (double.IsNegativeInfinity(left) && double.IsNegativeInfinity(right))
             {
                 return true;
             }
 
-            return right - left > -FloatPrecision;
+            return right - left > -DoublePrecision;
         }
     }
 }
